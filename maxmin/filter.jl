507335c8e4eb4c6326c963504b02aa176b60ec3d
marinka-bobidi <155455955+marinka-bobidi@users.noreply.github.com>
2024-11-28 02:58:48 +0300

    alg min-max

--- maxmin/filter.jl
+++ maxmin/filter.jl
@@ -1,59 +1,45 @@
+using DataStructures
 using Plots
 
-function max_min_filter_window(arr::Vector{Int}, w::Int)
-    comparisons_max = 0
-    comparisons_min = 0
-    n = length(arr)
-    max_vals = []
-    min_vals = []
-    U=[]
-    L=[]
+function max_min_filter_window(arr::Vector{Float64}, w::Int)
+    max_vals = fill(0.0, length(arr) - w + 1)
+    min_vals = fill(0.0, length(arr) - w + 1)
+    U = Deque{Int}()
+    L = Deque{Int}()
 
-    for i in 1:(n - w + 1)
-        window_max = -Inf
-        window_min = Inf
-
-        for j in i:(i + w - 1)
-            comparisons_max += 1
-            if arr[j] > window_max
-                window_max = arr[j]
-                push!(U, window_max)
-            end
-              for k in length(U):-1:2
-              if U[k]> U[k-1] 
-                deleteat!(U, k-1)
-              end
-            end
-
-
-            comparisons_min += 1
-            if arr[j] < window_min
-                window_min = arr[j]
-                push!(L, window_min)
-            end
-            for k in length(L):-1:2
-              if L[k]< L[k-1]
-                deleteat!(L, k-1)
-              end
-            end
+    for i in 1:length(arr)
+        if length(U) > 0 && first(U) <= i - w
+            popfirst!(U)
         end
+        if length(L) > 0 && first(L) <= i - w
+           popfirst!(L)
+        end
+        
+    
+        while length(U) > 0 && arr[last(U)] <= arr[i]
+            pop!(U)
+        end
+        while length(L) > 0 && arr[last(L)] >= arr[i]
+            pop!(L)
+        end
+        push!(U, i)
+        push!(L, i)
 
-        push!(max_vals, window_max)
-        push!(min_vals, window_min)
+        if i >= w
+            max_vals[i - w + 1] = arr[first(U)]
+            min_vals[i - w + 1] = arr[first(L)]
+        end
     end
-    println(U)
-    println(L)
-    println("Сравнений для макс: $comparisons_max, Сравнений для мин: $comparisons_min")
-    return max_vals, min_vals, U, L 
+
+    return max_vals, min_vals
 end
 
 # Пример использования функции
-arr = [7, 9, 3, 4, 7, 5, 3, 6, 2, 6]
+t = 0:0.1:10
+arr = sin.(t) * 2 +sin.(2t) * 4 + 4sin.(3t) * 5
 w = 3
-max_vals, min_vals, U,L= max_min_filter_window(arr, w)
+max_vals, min_vals = max_min_filter_window(arr, w)
 
 # Построение графика
-plot(U, label="Max Values", lw=2, seriestype=:scatter)
-plot!(L, label="Min Values", lw=2, seriestype=:scatter)
-
-
+scatter(max_vals, label="Max Values", lw=2, legend=:topleft)
+scatter!(min_vals, label="Min Values", lw=2, legend=:topleft)
