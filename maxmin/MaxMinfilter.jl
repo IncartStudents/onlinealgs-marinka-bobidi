372220442c9be094c48ceb35fc7c0b42506636eb
marinka-bobidi <155455955+marinka-bobidi@users.noreply.github.com>
2024-12-09 19:08:01 +0300

    fixxxxx

--- maxmin/MaxMinfilter.jl
+++ maxmin/MaxMinfilter.jl
@@ -3,47 +3,61 @@ module MaxMinFilter
 using DataStructures
 using Serialization
 
-export max_min_filter_window, save_state, load_state
+export FilterState, max_min_filter_online, init_filter_state, save_state, load_state
 
-function max_min_filter_window(arr::Vector{Float64}, w::Int)
-    len = length(arr) - w + 1
-    max_vals = fill(0.0, len)
-    min_vals = fill(0.0, len)
-    U = Deque{Int}()
-    L = Deque{Int}()
-
-    for i in 1:length(arr)
-        if length(U) > 0 && first(U) <= i - w
-            popfirst!(U)
-        end
-        if length(L) > 0 && first(L) <= i - w
-            popfirst!(L)
-        end
-
-        while length(U) > 0 && arr[last(U)] <= arr[i]
-            pop!(U)
-        end
-        while length(L) > 0 && arr[last(L)] >= arr[i]
-            pop!(L)
-        end
-        push!(U, i)
-        push!(L, i)
-
-        if i >= w
-            max_vals[i - w + 1] = arr[first(U)]
-            min_vals[i - w + 1] = arr[first(L)]
-        end
-    end
-
-    return max_vals, min_vals
+struct FilterState
+    arr::Vector{Float64}
+    max_vals::Vector{Float64}
+    min_vals::Vector{Float64}
+    U::Deque{Int}
+    L::Deque{Int}
+    w::Int
 end
 
-function save_state(filename::String, state)
+function init_filter_state(w::Int)
+    return FilterState([], [], [], Deque{Int}(), Deque{Int}(), w)
+end
+
+# Онлайн функция для фильтрации
+function max_min_filter_online(state::FilterState, new_value::Float64)
+    append!(state.arr, [new_value])
+    leng = length(state.arr)
+    U = state.U
+    L = state.L
+    w = state.w
+    if length(U) > 0 && first(U) <= leng - w
+        popfirst!(U)
+    end
+    if length(L) > 0 && first(L) <= leng - w
+        popfirst!(L)
+    end
+
+    while length(U) > 0 && state.arr[last(U)] <= new_value
+        pop!(U)
+    end
+    while length(L) > 0 && state.arr[last(L)] >= new_value
+        pop!(L)
+    end
+    push!(U, leng)
+    push!(L, leng)
+
+    if leng >= w
+        max_val = state.arr[first(U)]
+        min_val = state.arr[first(L)]
+        append!(state.max_vals, [max_val])
+        append!(state.min_vals, [min_val])
+    end
+
+    return state.max_vals, state.min_vals
+end
+
+# Функции для сохранения и загрузки состояния
+function save_state(filename::String, state::FilterState)
     serialize(filename, state)
 end
 
-function load_state(filename::String)
+function load_state(filename::String)::FilterState
     return deserialize(filename)
 end
 
-end
+end  
